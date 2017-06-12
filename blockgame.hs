5629d6633a93320765657cb13446441b9255dea6
Ensath <nfreed4@gmail.com>
2017-06-12 16:37:09 -0700

    Added types to remaining non-trivial functions

--- blockgame.hs
+++ blockgame.hs
@@ -51,11 +51,15 @@ victory = ["  Level",
 
 main = help
 info = help
+
+help :: IO()
 help = putStr "start levelN: Play the Nth level (currently there are six levels)\n\
               \startBig levelN: Play the Nth level with each game element represented by a 2x2 block of characters\n\
               \manual: Display the manual for the game\n\n"
 
 man = manual
+
+manual :: IO()
 manual = putStr "\
 \Welcome to Block Game!\n\
 \Move with WASD.\n\
@@ -67,26 +71,29 @@ manual = putStr "\
 \-: Open space\n\
 \=: Walls\n\n" 
 
+start :: [[Char]] -> IO a
 start state = do
  display state
  continue state
 
+continue :: [[Char]] -> IO a
 continue state = do
  c <- getChar
  display (move c state)
  continue (move c state)
- display (move c state)
 
+startBig :: [[Char]] -> IO a
 startBig state = do
  displayBig state
  continueBig state
 
+continueBig :: [[Char]] -> IO a
 continueBig state = do
  c <- getChar
  displayBig (move c state)
  continueBig (move c state)
- display (move c state)
 
+move :: Char -> [[Char]] -> [[Char]]
 move m state =
  if (m == 'a')
   then (moveLeft state)
@@ -96,10 +103,9 @@ move m state =
               then (moveUp state)
               else if(m=='s')
                     then (moveDown state)
-                    else if(m=='q')
-                          then state
-                          else state
+                    else state
 
+moveLeftRow :: [Char] -> [Char]
 moveLeftRow row = 
  if ("-O" `isInfixOf` row  || "!O" `isInfixOf` row) 
   then init(takeWhile(/= 'O') (row)) ++ "O-" ++ tail(dropWhile(/= 'O') (row));
@@ -107,8 +113,10 @@ moveLeftRow row =
         then init(init(takeWhile(/= 'O') row)) ++ "@O-" ++ tail(dropWhile(/= 'O') (row));
         else row
 
+moveLeft :: [[Char]] -> [[Char]]
 moveLeft state = map moveLeftRow state
 
+moveRightRow :: [Char] -> [Char]
 moveRightRow row = 
  if ("O-" `isInfixOf` row || "O!" `isInfixOf` row) 
   then (takeWhile(/= 'O') (row)) ++ "-O" ++ tail(tail(dropWhile(/= 'O') (row)));
@@ -116,8 +124,10 @@ moveRightRow row =
         then (takeWhile(/= 'O') (row)) ++ "-O@" ++ tail(tail(tail(dropWhile(/= 'O') (row))));
         else row
 
+moveRight :: [[Char]] -> [[Char]]
 moveRight state = map moveRightRow state
 
+moveUp :: [[Char]] -> [[Char]]
 moveUp state = 
  if (above 1 state == '-' || above 1 state == '!')
   then init(takeWhile (notElem 'O') state) ++ [enterRow state (aboveRow 1 state)] ++ [clearRow(head(dropWhile (notElem 'O') state))] ++ tail(dropWhile (notElem 'O') state)
@@ -127,6 +137,7 @@ moveUp state =
               then victory
               else state
 
+moveDown :: [[Char]] -> [[Char]]
 moveDown state = 
  if (below 1 state == '-' || below 1 state == '!')
   then takeWhile (notElem 'O') state ++ [clearRow(head(dropWhile (notElem 'O') state))] ++ [enterRow state (belowRow 1 state)] ++ tail(tail(dropWhile (notElem 'O') state))
@@ -134,19 +145,28 @@ moveDown state =
         then takeWhile (notElem 'O') state ++ [clearRow(head(dropWhile (notElem 'O') state))] ++ [enterRow state (belowRow 1 state)] ++ [pushRow state (belowRow 2 state)] ++ tail(tail(tail(dropWhile (notElem 'O') state)))
         else state 
 
+enterRow :: [[Char]] -> [Char] -> [Char]
 enterRow state row = take (findX state) row ++ "O" ++ drop (findX state + 1) row 
 
+clearRow :: [Char] -> [Char]
 clearRow row =
  if ("O" `isInfixOf` row) 
   then (takeWhile(/= 'O') (row)) ++ "-" ++ (tail(dropWhile(/= 'O') (row)));
   else row
 
+pushRow :: [[Char]] -> [Char] -> [Char]
 pushRow state row = take (findX state) row ++ "@" ++ drop (findX state + 1) row 
 
+above :: Int -> [[Char]] -> Char
 above n state = last(take (findX state + 1) (aboveRow n state))
+
+aboveRow :: Int -> [[Char]] -> [Char]
 aboveRow n state = last (take (findY state - n + 1) state)
 
+below :: Int -> [[Char]] -> Char
 below n state = last(take (findX state + 1) (belowRow n state))
+
+belowRow :: Int -> [[Char]] -> [Char]
 belowRow n state = last (take (findY state + n + 1) state)
 
 findX :: [[Char]] -> Int
